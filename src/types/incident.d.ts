2e68fbe9110babf2e7e44bcb32cc0d0a43119b1f
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-09-09 17:29:53 +0900

    add Incident type

---
+++ src/types/incident.d.ts
@@ -0,0 +1,9 @@
+type Incident = {
+    _path: string;
+    title: string;
+    componentsAffected: string[];
+    identified: string;
+    resolved: string;
+    severity: string;
+    steps: any[];
+};
