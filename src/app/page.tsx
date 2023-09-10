d10fcfdef5acc158c819d2d577b096a60dce145a
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-09-10 17:12:06 +0900

    add condition to the overall status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,23 +28,60 @@ export default async function Home() {
           yhakamay status
         </Link>
       </div>
-      <main className="flex min-h-screen flex-col items-center p-24">
-        <div className="alert alert-success">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="stroke-current shrink-0 h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>All systems operational</span>
-        </div>
+      <main className="flex min-h-screen max-w-screen-lg flex-col items-center p-12 md:p-24">
+        {countMajor < 1 && countMinor < 1 && countPotential < 1 ? (
+          <div className="alert alert-success">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>All systems operational</span>
+          </div>
+        ) : (
+          <div className="alert alert-warning">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 10V3L4 14h7v7l9-11h-7z"
+              />
+            </svg>
+            <span>
+              {countMajor > 0 && (
+                <span>
+                  {countMajor} major incident{countMajor > 1 && "s"}{" "}
+                </span>
+              )}
+              {countMinor > 0 && (
+                <span>
+                  {countMinor} minor incident{countMinor > 1 && "s"}{" "}
+                </span>
+              )}
+              {countPotential > 0 && (
+                <span>
+                  {countPotential} potential incident
+                  {countPotential > 1 && "s"}{" "}
+                </span>
+              )}
+              ongoing
+            </span>
+          </div>
+        )}
         {incidents.map((incident) => {
           const {
             _path,
