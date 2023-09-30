bf3474df93f0d2e0df7aa6aeb5814ac9f596a9d3
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-09-30 23:47:44 +0900

    add FilterButton component

---
+++ src/components/filter-button.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+
+export default function FilterButton() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams()!;
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  return (
+    <div className="dropdown dropdown-bottom dropdown-end">
+      <label tabIndex={0} className="btn m-1">
+        Severity
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+          className="fill-current"
+        >
+          <path d="M440-240q-17 0-28.5-11.5T400-280q0-17 11.5-28.5T440-320h80q17 0 28.5 11.5T560-280q0 17-11.5 28.5T520-240h-80ZM280-440q-17 0-28.5-11.5T240-480q0-17 11.5-28.5T280-520h400q17 0 28.5 11.5T720-480q0 17-11.5 28.5T680-440H280ZM160-640q-17 0-28.5-11.5T120-680q0-17 11.5-28.5T160-720h640q17 0 28.5 11.5T840-680q0 17-11.5 28.5T800-640H160Z" />
+        </svg>
+      </label>
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <Link href={pathname + "?" + createQueryString("severity", "all")}>
+            All
+          </Link>
+        </li>
+        <li>
+          <Link href={pathname + "?" + createQueryString("severity", "major")}>
+            Major
+          </Link>
+        </li>
+        <li>
+          <Link href={pathname + "?" + createQueryString("severity", "minor")}>
+            Minor
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={pathname + "?" + createQueryString("severity", "potential")}
+          >
+            Potential
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
