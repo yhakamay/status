b735bac39686e05fda8a4f9905c395de1b08ef86
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-10-01 02:23:23 +0900

    show resolved indicator at the top-right of cards

--- src/components/incident-card.tsx
+++ src/components/incident-card.tsx
@@ -6,8 +6,7 @@ type IncidentCardProps = {
 
 export default function IncidentCard(props: IncidentCardProps) {
   const { incident } = props;
-  const { title, scope, start, end, severity } = incident;
-  const resolved = end !== null;
+  const { title, scope, start, end, severity, resolved } = incident;
   const dateTimeOptions = {
     year: "numeric",
     month: "short",
@@ -19,49 +18,53 @@ export default function IncidentCard(props: IncidentCardProps) {
 
   return (
     <>
-      <div className="card w-full bg-base-100 shadow-md mt-2">
-        <Link href={`/incidents/${incident.slug}`}>
-          <div className="card-body">
-            <div className="flex justify-between pr-4">
-              <div>
-                <h1 className="font-semibold text-lg inline">{title}</h1>
-                {resolved && (
-                  <div className="badge badge-success ml-2">resolved</div>
-                )}
-                <span
-                  className={`badge ml-2 ${
-                    severity === "major"
-                      ? "badge-error"
-                      : severity === "minor"
-                      ? "badge-warning"
-                      : severity === "potential"
-                      ? "badge-info"
-                      : ""
-                  }`}
-                >
-                  {severity}
-                </span>
+      <div className="indicator w-full mt-6">
+        {resolved && (
+          <span className="indicator-item badge badge-success mr-6">
+            resolved
+          </span>
+        )}
+        <div className="card bg-base-100 shadow-md w-full">
+          <Link href={`/incidents/${incident.slug}`}>
+            <div className="card-body">
+              <div className="flex justify-between pr-4">
+                <div>
+                  <h1 className="font-semibold text-lg inline">{title}</h1>
+                  <span
+                    className={`badge ml-2 ${
+                      severity === "major"
+                        ? "badge-error"
+                        : severity === "minor"
+                        ? "badge-warning"
+                        : severity === "potential"
+                        ? "badge-info"
+                        : ""
+                    }`}
+                  >
+                    {severity}
+                  </span>
+                </div>
               </div>
+              {scope !== (null || undefined) && (
+                <p className="opacity-70">Scope: {scope.join(", ")}</p>
+              )}
+              {start !== (null || undefined) && (
+                <p className="opacity-70">
+                  Identified:{" "}
+                  {new Date(start).toLocaleString("en-US", dateTimeOptions)}
+                </p>
+              )}
+              {resolved !== (null || undefined) && (
+                <p className="opacity-70">
+                  Resolved:{" "}
+                  {resolved && end
+                    ? new Date(end).toLocaleString("en-US", dateTimeOptions)
+                    : "-"}
+                </p>
+              )}
             </div>
-            {scope !== (null || undefined) && (
-              <p className="opacity-70">Scope: {scope.join(", ")}</p>
-            )}
-            {start !== (null || undefined) && (
-              <p className="opacity-70">
-                Identified:{" "}
-                {new Date(start).toLocaleString("en-US", dateTimeOptions)}
-              </p>
-            )}
-            {resolved !== (null || undefined) && (
-              <p className="opacity-70">
-                Resolved:{" "}
-                {resolved
-                  ? new Date(end).toLocaleString("en-US", dateTimeOptions)
-                  : "-"}
-              </p>
-            )}
-          </div>
-        </Link>
+          </Link>
+        </div>
       </div>
     </>
   );
